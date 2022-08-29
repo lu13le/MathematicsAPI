05718c0eb15b4f47950908f11d82432c6fce71a5
luka.raicevic <luka.raicevic@NB-SRB-IT-01>
2022-08-29 12:47:52 +0200

    Add project files.

---
+++ MathematicsAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using MathematicsAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathematicsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : Controller
+    {
+        private readonly ICalculateRepo _calculateRepo;
+
+        public CalculatorController(ICalculateRepo calculateRepo)
+        {
+            _calculateRepo=calculateRepo;
+        }
+       
+        [HttpPost("Add")]
+        public IActionResult Add(int a, int b)
+        {
+
+            var result = _calculateRepo.Add(a, b);
+            return Ok(result);
+        }
+        [HttpPost("Substract")]
+        public IActionResult Substract(int a, int b)
+        {
+            var result = _calculateRepo.Substract(a, b);
+            return Ok(result);
+        }
+        [HttpPost("Divide")]
+        public IActionResult Divide(int a, int b)
+        {
+            var result = _calculateRepo.Divide(a, b);
+            return Ok(result);
+        }
+        [HttpPost("Multiply")]
+        public IActionResult Multiply(int a, int b)
+        {
+            var result = _calculateRepo.Multiply(a, b);
+            return Ok(result);
+        }
+    }
+}
