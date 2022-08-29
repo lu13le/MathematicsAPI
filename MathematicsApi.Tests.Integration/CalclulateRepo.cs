78fdfdc65c807fe0fecca6b0507d72c60bde4638
ana.asceric <ana.asceric@NB-SRB-IT-03>
2022-08-29 15:02:38 +0200

    ,

---
+++ MathematicsApi.Tests.Integration/CalclulateRepo.cs
@@ -0,0 +1,40 @@
+﻿namespace MathematicsAPI.Services
+{
+    public class CalclulateRepo : ICalculateRepo
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            if(x==0 || y == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return x / y;
+            }
+           
+        }
+
+        public double Multiply(double x, double y)
+        {
+            if (x == 0 || y == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return x * y;
+            }
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ MathematicsApi.Tests.Integration/ICalculateRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace MathematicsAPI.Services
+{
+    public interface ICalculateRepo
+    {
+        double Add(double x, double y);
+        double Substract(double x, double y);
+        double Multiply(double x, double y);
+        double Divide(double x, double y);
+
+    }
+}
